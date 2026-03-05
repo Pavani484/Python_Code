a1c59717742532d910c1e0249a43001e508510f0
pavani <pavanigenamala@gmail.com>
2026-03-05 23:03:12 +0530

    In dynamic.py  file changes are done

--- dynamic.py
+++ dynamic.py
@@ -4,3 +4,14 @@ for i in range(n,0,-1):
     fact=fact*i
 print(fact)
 #................
+n=int(input("enter a number:"))
+n1=int(input("enter a number:"))
+for i in range(n,n1+1):
+    c=0
+    for j in range(1,i+1):
+        if i%j==0:
+            c+=1
+    if c==2:
+        print(i)
+#.....................
+
