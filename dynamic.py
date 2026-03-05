3a0cd14590143bcdeb85478358730d0ffcdaa759
pavani <pavanigenamala@gmail.com>
2026-03-05 23:04:09 +0530

    In dynamic.py  file changes are done

--- dynamic.py
+++ dynamic.py
@@ -14,4 +14,12 @@ for i in range(n,n1+1):
     if c==2:
         print(i)
 #.....................
-
+n=int(input("enter a number:"))
+n1=0
+n2=1
+for i in range(1,n+1):
+    print(n1,end=" ")
+    t=n1+n2
+    n1=n2
+    n2=t
+#........................
