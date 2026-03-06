5454287baecc5b5bc0cb986ae2aa693a9851524d
pavani <pavanigenamala@gmail.com>
2026-03-06 20:03:50 +0530

    first.py file is added

---
+++ first.py
@@ -0,0 +1,9 @@
+# from second import *
+# m2(int(input("Enter a number: ")))
+# a, b = map(int, input("Enter two numbers with space: ").split())
+# add(a,b)
+# sub(a,b)
+# mul(a,b)
+# div(a,b)
+#............................
+
