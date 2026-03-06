217024d83eecd9aaf13750fc9e9c6b00ee8be4ab
pavani <pavanigenamala@gmail.com>
2026-03-06 20:06:09 +0530

    first.py file  changes are updated

--- first.py
+++ first.py
@@ -6,4 +6,33 @@
 # mul(a,b)
 # div(a,b)
 #............................
+# num=int(input("Enter a number: "))
+# prime(num)
+#.....................
+# n=int(input("Enter a number : "))
+# li=[]
+# for i in range(n):
+#     a=int(input("Enter elements into list : "))
+#     li.append(a)
+# print(li)
+# p=int(input("Enter a position to insert element : "))
+# e=int(input("Enter an element : "))
+# li.insert(p-1,e)
+# print(li)
+# e1=int(input("Enter an element : "))
+# if e1 in li:
+#     li.remove(e1)
+#     print(li)
+# else:
+#     print("element is not found in list")
+# p1=int(input("Enter a position to remove: "))
+# if p1 > 0 and p1 <= len(li):
+#     li.pop(p1)
+# print(li)
+# e2=int(input("Enter an element to print the position: "))
+# if e2 in li:
+#     print(li.index(e2))
+# else:
+#     print("element is not found in list")
+#....................................
 
