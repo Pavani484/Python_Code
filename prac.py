6e367ac18cfcd5608b420c9502ff4a45a773a38c
pavani <pavanigenamala@gmail.com>
2026-03-07 19:14:06 +0530

    prac.py file changes are done

--- prac.py
+++ prac.py
@@ -20,3 +20,44 @@
 # print(b)
 # print(type(b))
 
+# a="pavani"
+# print(a)
+# print(type(a))
+# b=int(a)
+# print(b)
+# print(type(b))
+# bool to int
+# a=True
+# print(a)
+# print(type(a))
+# b=int(a)
+# print(b)
+# print(type(b))
+#complex to int
+# a=10-4j
+# print(a)
+# print(type(a))
+# b=int(a)
+# print(b)
+# print(type(b))
+# print("float conversion")
+#int to float
+# a=10
+# print(a)
+# print(type(a))
+# b=float(a)
+# print(b)
+# print(type(b))
+#float to float
+# a=10.45
+# print(a)
+# print(type(a))
+# b=float(a)
+# print(b)
+# print(type(b))
+#str to float
+# a="wish"
+# print(a)
+# print(type(a))
+# b=float(a)
+# print(b)
