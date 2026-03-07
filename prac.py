6e35a4fdc804ebb681af8e497c9ffc69aed59e1e
pavani <pavanigenamala@gmail.com>
2026-03-07 19:12:29 +0530

    prac.py file is added

---
+++ prac.py
@@ -0,0 +1,22 @@
+# #int to int
+# a=10
+# print(a)
+# print(type(a))
+# b=int(a)
+# print(b)
+# print(type(b))
+#float to int
+# a=10.56
+# print(a)
+# print(type(a))
+# b=int(a)
+# print(b)
+# print(type(b))
+#str to int
+# a=""
+# print(a)
+# print(type(a))
+# b=int(a)
+# print(b)
+# print(type(b))
+
