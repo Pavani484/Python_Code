c36faa0683a10fba8aea379c075480f0e1b954dd
pavani <pavanigenamala@gmail.com>
2026-03-11 09:49:31 +0530

    realcode.py code is commited

--- realcode.py
+++ realcode.py
@@ -16,8 +16,24 @@
 #         let_max=l1[i]
 # print(let_max)
 #........................
-
-
+#How do you remove duplicate elements from a list without using set()
+# li=[9,4,5,6,6]
+# l=[]
+# for i in li:
+#     if i not in l:
+#         l.append(i)
+# print(l)
+#(or)
+# li = [9,4,5,6,6]
+# unique = []
+# seen = {}
+# for num in li:
+#     if num not in seen:
+#         unique.append(num)
+#         seen[num] = True
+#         print(seen)
+# print(unique)
+# ..................................
 
 
 
