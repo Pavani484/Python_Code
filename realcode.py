7b0f67a088583f6167a5070e9b5b61dc786ead48
pavani <pavanigenamala@gmail.com>
2026-03-11 09:48:42 +0530

    realcode.py file is commited

---
+++ realcode.py
@@ -0,0 +1,31 @@
+#1.	Write a program to store n product prices in a list and find the maximum price
+# n=int(input("enter a number of product prices:"))
+# l1=[]
+# for i in range(n):
+#     l=int(input("enter a product price:"))
+#     l1.append(l)
+# print(max(l1))
+# #(or)
+# n=int(input("enter a number of product prices:"))
+# l1=[]
+# let_max=float('-inf')
+# for i in range(n):
+#     l=int(input("enter a product price:"))
+#     l1.append(l)
+#     if l1[i]>let_max:
+#         let_max=l1[i]
+# print(let_max)
+#........................
+
+
+
+
+
+
+
+
+
+
+
+
+
