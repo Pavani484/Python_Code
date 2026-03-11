b53c9259846ac8f4c96efa438c65de85476348a2
pavani <pavanigenamala@gmail.com>
2026-03-11 09:46:45 +0530

    second.py code is commited

--- second.py
+++ second.py
@@ -13,8 +13,15 @@
 #     print(f"multiplication of {a} and {b} is : {a*b}")
 # def div(a,b):
 #     print(f"division of {a} and {b} is : {a/b}")
-
-
+#..........................
+# def prime(num):
+#     for i in range(2,num):
+#         if num % i==0:
+#             print("not a prime")
+#             break
+#     else:
+#         print("prime")
+#......................
 
 
 
