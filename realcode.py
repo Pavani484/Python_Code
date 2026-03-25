6fa0227f9b730a461b123c981ba9755f376b0904
pavani <pavanigenamala@gmail.com>
2026-03-25 20:28:50 +0530

    code is added

--- realcode.py
+++ realcode.py
@@ -625,12 +625,13 @@ from jinja2.nodes import Break
 #     print("Not Palindrome")
 #.......................
 #Search an element and display its position
-li=[2,3,5,6,7,8]
-s=int(input("enter an element:"))
-for i in range (len(li)):
-    if li[i]==s:
-        print("position of element in list is:",i)
-        break
-else:
-    print("element is not found in list")
+# li=[2,3,5,6,7,8]
+# s=int(input("enter an element:"))
+# for i in range (len(li)):
+#     if li[i]==s:
+#         print("position of element in list is:",i)
+#         break
+# else:
+#     print("element is not found in list")
+# ................
 
