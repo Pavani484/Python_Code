3079b7892950f6666c83a96d3daf7c12452fc6c7
pavani <pavanigenamala@gmail.com>
2026-03-27 20:08:14 +0530

    code is added

--- realcode.py
+++ realcode.py
@@ -678,3 +678,15 @@ from jinja2.nodes import Break
 # else:
 #     print("all elements are unique")
 #......................
+#Flatten a tuple of tuples
+t=((1,2),(4,5),(5,6))
+re=[]
+for i in t:
+    for j in i:
+        re.append(j)
+print(tuple(re))
+#or
+r=()
+for i in t:
+    r=r+i
+print(r)
