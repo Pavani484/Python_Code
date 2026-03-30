4baff78749a7b1ab602d15f4415ad18ba34e1760
pavani <pavanigenamala@gmail.com>
2026-03-30 18:45:10 +0530

    code is added

--- realcode.py
+++ realcode.py
@@ -788,12 +788,15 @@ from jinja2.nodes import Break
 # print(len(s2))
 #.....................
 #Remove vowels from set of characters
-s = {'a','b','c','e','i','o','u','d','f'}
-for ch in {'a','e','i','o','u'}:
-    if ch in s:
-        s.remove(ch)
-print(s)
+# s = {'a','b','c','e','i','o','u','d','f'}
+# for ch in {'a','e','i','o','u'}:
+#     if ch in s:
+#         s.remove(ch)
+# print(s)
 #..............
+
+
+
 
 
 
