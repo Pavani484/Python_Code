2b245c8172c91b52170af9ff28346239db4c5bbf
pavani <pavanigenamala@gmail.com>
2026-04-11 21:28:45 +0530

    code is added

--- second.py
+++ second.py
@@ -35,57 +35,7 @@
 # """)
 # cursor.execute("INSERT INTO students VALUES (?, ?, ?, ?)", (1, "Alice", 14, "A"))
 # cursor.execute("INSERT INTO students VALUES (?, ?, ?, ?)", (2, "Bob", 15, "B"))
-# cursor.execute("INSERT INTO students VALUES (?, ?, ?, ?)", (3, "Charlie", 14, "A"))
-# cursor.execute("INSERT INTO students VALUES (?, ?, ?, ?)", (4, "pavani", 16, "A"))
-# conn.commit()
-# cursor.execute("SELECT * FROM students")
-# print("Initial Records:")
-# for row in cursor.fetchall():
-#     print(row)
-# cursor.execute("UPDATE students SET grade = ? WHERE name = ?", ("B", "Bob"))
-# cursor.execute("DELETE FROM students WHERE id = ?", (3,))
-# conn.commit()
-# cursor.execute("SELECT * FROM students")
-# print("After Update and Delete:")
-# for row in cursor.fetchall():
-#     print(row)
-# cursor.close()
-# conn.close()
-#......................
-# import mysql.connector
-# conn = mysql.connector.connect(
-# host="localhost",
-# user="root",
-# password="Pavani@9312",
-# database="college"
-# )
-# cursor = conn.cursor()
-# cursor.execute("""
-# CREATE TABLE IF NOT EXISTS students (
-# id INT PRIMARY KEY,
-# name VARCHAR(100) NOT NULL,
-# age INT,
-# grade VARCHAR(2)
-# )
-# """)
-# cursor.execute("INSERT INTO students VALUES (%s, %s, %s, %s)", (1, "Alice", 14, "A"))
-# cursor.execute("INSERT INTO students VALUES (%s, %s, %s, %s)", (2, "Bob", 15, "B"))
-# cursor.execute("INSERT INTO students VALUES (%s, %s, %s, %s)", (3, "Charlie", 14, "A"))
-# conn.commit()
-# cursor.execute("SELECT * FROM students")
-# print("Initial Records:")
-# for row in cursor.fetchall():
-#     print(row)
-# cursor.execute("UPDATE students SET grade = %s WHERE name = %s", ("A+", "Bob"))
-# cursor.execute("DELETE FROM students WHERE id = %s", (3,))
-# conn.commit()
-# cursor.execute("SELECT * FROM students")
-# print("After Update and Delete:")
-# for row in cursor.fetchall():
-#     print(row)
-# cursor.close()
-# conn.close()
-#..........................
+
 
 
 
