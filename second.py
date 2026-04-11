490f048a3aca106d230382b8d108a192fe4b10d7
pavani <pavanigenamala@gmail.com>
2026-04-11 20:04:01 +0530

    code is added

--- second.py
+++ second.py
@@ -33,3 +33,18 @@
 # grade TEXT
 # )
 # """)
+# cursor.execute("INSERT INTO students VALUES (?, ?, ?, ?)", (1, "Alice", 14, "A"))
+# cursor.execute("INSERT INTO students VALUES (?, ?, ?, ?)", (2, "Bob", 15, "B"))
+# cursor.execute("INSERT INTO students VALUES (?, ?, ?, ?)", (3, "Charlie", 14, "A"))
+# cursor.execute("INSERT INTO students VALUES (?, ?, ?, ?)", (4, "pavani", 16, "A"))
+# conn.commit()
+# cursor.execute("SELECT * FROM students")
+# print("Initial Records:")
+# for row in cursor.fetchall():
+#     print(row)
+# cursor.execute("UPDATE students SET grade = ? WHERE name = ?", ("B", "Bob"))
+# cursor.execute("DELETE FROM students WHERE id = ?", (3,))
+# conn.commit()
+
+
+
