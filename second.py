5c03df16c9ab882836b36b8c64b69ede67a686df
pavani <pavanigenamala@gmail.com>
2026-04-11 20:04:55 +0530

    code is added

--- second.py
+++ second.py
@@ -45,6 +45,15 @@
 # cursor.execute("UPDATE students SET grade = ? WHERE name = ?", ("B", "Bob"))
 # cursor.execute("DELETE FROM students WHERE id = ?", (3,))
 # conn.commit()
+# cursor.execute("SELECT * FROM students")
+# print("After Update and Delete:")
+# for row in cursor.fetchall():
+#     print(row)
+# cursor.close()
+# conn.close()
+#......................
+
+
 
 
 
