168ca9fe62575c7ebb0d4b7aa92cc24a6a7032f5
pavani <pavanigenamala@gmail.com>
2026-04-11 20:03:11 +0530

    code is added

--- second.py
+++ second.py
@@ -22,6 +22,14 @@
 #     else:
 #         print("prime")
 #.....................
-
-
-
+# import sqlite3
+# conn = sqlite3.connect("school.db")
+# cursor = conn.cursor()
+# cursor.execute("""
+# CREATE TABLE IF NOT EXISTS students (
+# id INTEGER PRIMARY KEY,
+# name TEXT NOT NULL,
+# age INTEGER,
+# grade TEXT
+# )
+# """)
