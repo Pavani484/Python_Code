c266d9fb1efeff053bf57f55afa7253fe25b2412
pavani <pavanigenamala@gmail.com>
2026-04-14 20:02:32 +0530

    code is commited

--- Atm.py
+++ Atm.py
@@ -248,3 +248,16 @@ def dashboard():
     tk.Label(root, text="Old PIN", bg="#1e1e2f", fg="white").pack()
     old_pin = tk.Entry(root)
     old_pin.pack()
+
+    tk.Label(root, text="New PIN", bg="#1e1e2f", fg="white").pack()
+    new_pin = tk.Entry(root)
+    new_pin.pack()
+
+    tk.Button(root, text="Change PIN", command=change_pin).pack(pady=5)
+    tk.Button(root, text="History", command=show_history).pack(pady=5)
+    tk.Button(root, text="Logout", bg="red", fg="white", command=logout).pack(pady=10)
+
+
+# Start App
+login_screen()
+root.mainloop()
