6cb46636f837209d9544453d1ee0a22a6548edae
pavani <pavanigenamala@gmail.com>
2026-04-24 19:11:51 +0530

    code is commited

--- DS.py
+++ DS.py
@@ -5,9 +5,16 @@ data={
     'marks':[40,45,46,47]
 }
 df=pd.DataFrame(data)
-print(df[['name','marks']])
-print(df[df['age']>22])
-print(df[df['marks']>43])
+# print(df[['name','marks']])
+# print(df[df['age']>22])
+# print(df[df['marks']>43])
+# print(df[(df['age'] > 21) & (df['marks'] >= 45)])
+# print(df[(df['age']>=25) & (df['marks']>45)])
+def check_result(x):
+    if x >= 45:
+        return 'Pass'
+    else:
+        return 'Fail'
 
-print(df[(df['age'] > 21) & (df['marks'] >= 45)])
-print(df[(df['age']>=25) & (df['marks']>45)])
+df['Result'] = df['marks'].apply(check_result)
+print(df)
