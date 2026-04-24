335a0a7d3de865b7142630331ea98a2387b1c526
pavani <pavanigenamala@gmail.com>
2026-04-24 19:17:18 +0530

    code is commited

--- DS.py
+++ DS.py
@@ -10,20 +10,20 @@ df=pd.DataFrame(data)
 # print(df[df['marks']>43])
 # print(df[(df['age'] > 21) & (df['marks'] >= 45)])
 # print(df[(df['age']>=25) & (df['marks']>45)])
-def check_result(x):
-    if x >= 45:
-        return 'Pass'
-    else:
-        return 'Fail'
-
-df['Result'] = df['marks'].apply(check_result)
-print(df)
-def check_grade(a):
-    if a>=46:
-        return 'A'
-    elif a>=45:
-        return 'B'
-    else:
-        return 'C'
-df['Grade']=df['marks'].apply(check_grade)
-print(df)
+# def check_result(x):
+#     if x >= 45:
+#         return 'Pass'
+#     else:
+#         return 'Fail'
+#
+# df['Result'] = df['marks'].apply(check_result)
+# print(df)
+# def check_grade(a):
+#     if a>=46:
+#         return 'A'
+#     elif a>=45:
+#         return 'B'
+#     else:
+#         return 'C'
+# df['Grade']=df['marks'].apply(check_grade)
+# print(df)
