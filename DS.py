cefba32c143426c738687ac83cbcaf727f071817
pavani <pavanigenamala@gmail.com>
2026-04-24 18:41:49 +0530

    code is commited

---
+++ DS.py
@@ -0,0 +1,8 @@
+import pandas as pd
+data={
+    'name':['pavani','lalli','pavana','cherry'],
+    'age':[20,23,25,26],
+    'marks':[40,45,46,47]
+}
+df=pd.DataFrame(data)
+print(df[['name','marks']])
